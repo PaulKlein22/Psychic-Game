040f7952b49312a4d6e5375fb55167ffaafd2ecc
Elrood22 <paulklein22@gmail.com>
2018-09-20 18:15:05 -0500

    final

---
+++ Assets/javascript/game.js
@@ -0,0 +1,28 @@
+var letters = "abcdefghijklmnopqrstuvwxyz".split("");
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === randomLetter){
+            wins++;
+            guesses = 10
+            randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    } 
+    else {
+            guesses--;
+    }
+    if(guesses === 0){
+            losses++
+            guesses = 10
+            randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guesses").innerHTML = "Remaining Guesses: " + guesses;
+    document.getElementById("chosen").innerHTML = "Your Guesses So Far: " + userGuess;
+}
